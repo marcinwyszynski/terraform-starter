d11dfab6261c51a185671ff25a8061b1f7b66014
Marcin Wyszynski <marcin.pixie@gmail.com>
2021-04-01 09:51:35 +0200

    Revert "Test multi-endpoint support (#6)"
    This reverts commit c7a80295f71ee49506fb4fa8d20e34d656c4fbea.

--- main.tf
+++ main.tf
@@ -1,25 +1,7 @@
-variable "spacelift_key_id" {}
-variable "spacelift_key_secret" {}
-
 terraform {
   required_providers {
     spacelift = {
       source = "downloads.spacelift.dev/spacelift-io/spacelift"
     }
   }
-}
-
-provider "spacelift" {}
-
-provider "spacelift" {
-  alias = "external"
-
-  api_key_endpoint = "https://spacelift-io.app.spacelift.dev"
-  api_key_id       = var.spacelift_key_id
-  api_key_secret   = var.spacelift_key_secret
-}
-
-resource "spacelift_context" "externally-managed" {
-  provider = spacelift.external
-  name     = "Externally created context"
 }
